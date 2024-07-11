e0ab64531e6e76f9c41d99b50142f9a67f966fdb
FujiwaraChoki <fuji@fuji.codes>
2024-07-11 21:45:01 +0200

    feat: Add exception handler

--- src/main/java/ch/zli/m223/controller/ticketing/TicketingAdminController.java
+++ src/main/java/ch/zli/m223/controller/ticketing/TicketingAdminController.java
@@ -1,8 +1,11 @@
 package ch.zli.m223.controller.ticketing;
 
+import ch.zli.m223.exception.BookingNotFoundException;
 import ch.zli.m223.model.Booking;
 import ch.zli.m223.service.ticketing.TicketingAdminService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -18,5 +21,13 @@ public class TicketingAdminController {
     @DeleteMapping("/bookings_a")
     public void deleteBooking(@RequestBody Long id) {
         ticketingAdminService.suspendBooking(id);
+    }
+
+    @ExceptionHandler(BookingNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseEntity<String> handleBookingNotFoundException(BookingNotFoundException e) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(e.getMessage());
     }
 }
