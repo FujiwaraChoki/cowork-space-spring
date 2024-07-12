9608c78e955da12388c0ad36a750e96fa832d2c1
FujiwaraChoki <fuji@fuji.codes>
2024-07-12 09:42:28 +0200

    feat: add openapi specifications

--- src/main/java/ch/zli/m223/controller/ticketing/TicketingAdminController.java
+++ src/main/java/ch/zli/m223/controller/ticketing/TicketingAdminController.java
@@ -8,10 +8,12 @@ import ch.zli.m223.service.ticketing.TicketingAdminService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/admin/ticketing")
+@Transactional
 public class TicketingAdminController {
     public TicketingAdminController(TicketingAdminService ticketingAdminService, BookingRepository bookingRepository) {
         this.ticketingAdminService = ticketingAdminService;
